6a6b6d578f0abe9dad78633758ce30b1f2307e00
lordkevinmo <kevagb03@gmail.com>
2019-06-13 16:43:29 +0200

    re-update constraints

--- src/main/generated-sources/sarl/fr/utbm/info/ia51/ar/requirements/Constraints.java
+++ src/main/generated-sources/sarl/fr/utbm/info/ia51/ar/requirements/Constraints.java
@@ -15,14 +15,44 @@ import org.eclipse.xtext.xbase.lib.Pure;
 public class Constraints {
   private ConstrainsType type;
   
-  public Constraints(final ConstrainsType constraint) {
+  private int influence;
+  
+  public Constraints(final ConstrainsType constraint, final int influence) {
     this.type = constraint;
+    this.influence = influence;
+  }
+  
+  @Pure
+  public ConstrainsType getType() {
+    return this.type;
+  }
+  
+  public void setType(final ConstrainsType type) {
+    this.type = type;
+  }
+  
+  @Pure
+  public int getInfluence() {
+    return this.influence;
+  }
+  
+  public void setInfluence(final int influ) {
+    this.influence = influ;
   }
   
   @Override
   @Pure
   @SyntheticMember
   public boolean equals(final Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Constraints other = (Constraints) obj;
+    if (other.influence != this.influence)
+      return false;
     return super.equals(obj);
   }
   
@@ -31,6 +61,8 @@ public class Constraints {
   @SyntheticMember
   public int hashCode() {
     int result = super.hashCode();
+    final int prime = 31;
+    result = prime * result + this.influence;
     return result;
   }
 }
