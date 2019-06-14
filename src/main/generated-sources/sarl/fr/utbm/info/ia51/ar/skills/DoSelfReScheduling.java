3f1a0f526ba6e0d170421797808dbcb464a2ceef
dev_gedeon <cyrilleagotsi@gmail.com>
2019-06-14 20:55:20 +0200

    Modify pertubation event and event UI.

--- src/main/generated-sources/sarl/fr/utbm/info/ia51/ar/skills/DoSelfReScheduling.java
+++ src/main/generated-sources/sarl/fr/utbm/info/ia51/ar/skills/DoSelfReScheduling.java
@@ -1,6 +1,6 @@
 package fr.utbm.info.ia51.ar.skills;
 
-import fr.utbm.info.ia51.ar.capacities.SelfReScheduling;
+import fr.utbm.info.ia51.ar.capacities.SocialReScheduling;
 import fr.utbm.info.ia51.ar.requirements.Episode;
 import io.sarl.lang.annotation.SarlElementType;
 import io.sarl.lang.annotation.SarlSpecification;
@@ -8,10 +8,12 @@ import io.sarl.lang.annotation.SyntheticMember;
 import io.sarl.lang.core.Agent;
 import io.sarl.lang.core.Skill;
 import java.util.List;
+import java.util.Map;
+import org.eclipse.xtext.xbase.lib.Pure;
 
 /**
  * <p>
- * This sill implements the capacity of the agent
+ * This skill implements  the capacity of the agent
  * to self Re-schedule following the reception of
  * constraining event
  * </p>
@@ -21,8 +23,15 @@ import java.util.List;
 @SarlSpecification("0.9")
 @SarlElementType(22)
 @SuppressWarnings("all")
-public class DoSelfReScheduling extends Skill implements SelfReScheduling {
+public class DoSelfReScheduling extends Skill implements SocialReScheduling {
+  @Pure
+  public void makeSocialRescheduling(final Map<String, List<Episode>> participantEpisode) {
+    throw new UnsupportedOperationException("TODO: auto-generated method stub");
+  }
+  
+  @Pure
   public void makeRescheduling(final List<Episode> episodes) {
+    throw new UnsupportedOperationException("TODO: auto-generated method stub");
   }
   
   @SyntheticMember
